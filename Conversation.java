a995c043c4d4fd508fc748211917ec1de21df59c
Your Name <yzhu06@smith.edu>
2024-02-12 11:03:37 -0500

    Finish writing most of the codes, but missing the last part

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,57 @@
-class Conversation {
+import java.util.Scanner; 
+import java.util.ArrayList;
+import java.util.List;
+class Conversation{
 
   public static void main(String[] arguments) {
+    Scanner sc = new Scanner (System.in);
+
+    System.out.println("Please enter how many rounds you want the conversation to go:");
+    System.out.println("Hi, how are you doing today");
+    int numRounds = sc.nextInt();
+    System.out.println("Please enter the start of the conversation:");
+    for (int i = 0; i -1 < numRounds; i++){
+      String userInput = sc.nextLine();
+      String[] wordsArray = userInput.split(" ");
+      List<String> wordsList = new ArrayList<>();
+      String response = "";
+      for (String word : wordsArray) {
+        wordsList.add(word);
+    }
+    
+      List<String> sentence = new ArrayList<>();
+          boolean containswords = true;
+          for (String word:wordsList) {
+            if (word.equals("me")) {
+            } else if (word.equals("I")) {
+                word = "you";
+                
+            } else if (word.equals("My")) {
+                word = "Your";
+            } else if (word.equals("my")) {
+                word = "your";
+            } else if (word.equals("Mine")) {
+                word = "Yours";
+            } else if (word.equals("mine")) {
+                word = "yours";
+            } else if (word.equals("am")){
+                word = "are"; 
+            }
+
+            sentence.add(word);
+          
+          
+
+
+        }
+      for (String word1:sentence){
+        response += word1 + " ";
+      }
+      response +=".";
+      System.out.println(response);
+      
+
+}}}
+      
     // You will start the conversation here.
-  }
-}
+ 
